f5d251930504a6389ec56f01f553fb712b247015
FMilenkovic03 <fmilenkovic03@gmail.com>
2024-09-04 14:18:39 +0200

    creation of consumer

--- main.c
+++ main.c
@@ -1,7 +1,41 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(){
-    int a;
-    return 0;
+//Add new records 
+//View list of records 
+//Modify records 
+//View payment 
+//Search Records 
+//Delete records
+
+
+struct Consumer{
+    char name[50]; 
+    char phoneNumber[15]; 
+    float usage; 
+    float totalBill; 
+};
+
+struct Consumer consumers[100]; 
+
+
+int consumerCount = 0;
+
+void addConsumer(){
+    struct Consumer newConsumer; 
+
+    printf("Ajoutez le nom du client : ");
+    anf(" %[^\n]", newConsumer.name);
+
+    printf("Entrez le numéro de téléphone: ");
+    scanf(" %s", newConsumer.phoneNumber);
+
+    printf("Entrez l'utilisation de forfait (Go): ");
+    scanf("%f", &newConsumer.usage);
+
+    newConsumer.totalBill = newConsumer.usage * 0.15;
+
+    
+    
 }
